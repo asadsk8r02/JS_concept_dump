a33eb83e1091d4a0d4c52372d2bccec067076ac3
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2023-08-05 16:06:23 -0500

    Add files via upload

---
+++ array_quest.js
@@ -0,0 +1,43 @@
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let a = prompt("Enter a number")
+// a = Number.parseInt(a)
+// arr.push(a)
+
+// let arr = [1,2,3,4,5,6,7,8,9]
+// let a;
+// do {
+//     a = prompt("Enter a number")
+//     a = Number.parseInt(a)
+//     arr.push(a)
+//     console.log(arr)
+// }while(a!=0);
+
+// console.log(arr)
+
+// let arr = [1,2,3,4,5,6,7,8,9,60,20,20,30,40]
+
+// let n = arr.filter((a)=>{
+//     return a%10==0
+// })
+// console.log(n)
+
+// let sqr = arr.map((ele)=>{
+//     return (ele*ele)
+// })
+// console.log(sqr)
+
+// let inp = prompt("enter the number")
+// imp = Number.parseInt(inp)
+// let ar = [];
+// for(let i =0; i<=imp; i++){
+//     ar.push(i)
+// }
+// ar = [1,2,3,4,5]
+
+// const factorial = (a,b)=>{
+//     return a*b
+// }
+// let a = ar.reduce(factorial)
+// console.log(a)
+
+ 
