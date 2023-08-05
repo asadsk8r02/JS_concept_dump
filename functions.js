712330331e66b4077666006470d369d66e9dae8d
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2023-08-05 16:05:24 -0500

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,25 @@
+let a=1
+let b=2
+let c=3
+
+function add(x,y,z){
+    return x+y+z
+}
+
+const sum = (p,q,r)=>{
+    return p+q+r
+}
+
+console.log(sum(a,b,c))
+
+const hello = ()=>{
+    console.log("Hello asad")
+    return "Hi Asad"
+}
+
+hello();
+
+let v = hello();
+console.log(v)
+
+
