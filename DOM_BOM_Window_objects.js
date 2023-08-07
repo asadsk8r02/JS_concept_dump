3bc147cb43844e9d14126e21c07e19056218b299
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2023-08-07 18:49:08 -0500

    Add files via upload

---
+++ DOM_BOM_Window_objects.js
@@ -0,0 +1,13 @@
+// Window ek global object hai and respresents browser window - 
+// everything comes inside window object - DOM, BOM and JS_objects
+
+// DOM - Document object model - Pure HTML page ko ek JS object bana 
+// diya gaya hai and usse "Document" bola hai
+
+window.console.log(window)
+console.log(document)
+console.log(document.body)
+
+// Browser object model - BOM - represent additional object provided by 
+// browser - alert, prompt and confirm are a part of BOM
+// example - location href - direct to different link is a part of BOM
