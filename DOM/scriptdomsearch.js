30c8d32c585d289d04db693a8c84f8cbdcbb517a
Asadullah Khan <53692166+asadsk8r02@users.noreply.github.com>
2023-08-09 01:28:29 -0500

    Add files via upload

---
+++ DOM/scriptdomsearch.js
@@ -0,0 +1,15 @@
+// let ctitle = document.getElementsByClassName("card-title")[0]
+// ctitle.style.color = "red"
+
+let ctitle = document.getElementById("firstcardtitle")
+ctitle.style.color = "blue"
+
+let ctitles = document.querySelectorAll(".card-title ")
+ctitle[0].style.color = "blue"
+ctitle[1].style.color = "green"
+ctitle[2].style.color = "red"
+comsole.log(ctitles)
+
+
+
+
